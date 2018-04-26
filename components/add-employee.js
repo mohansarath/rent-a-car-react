c90373cbbdac4e3898cdc86700a86de56b4c9ae9
sarath <sarath.sct@gmail.com>
2018-04-26 15:01:32 +0530

    Validation for add employee

--- components/add-employee.js
+++ components/add-employee.js
@@ -10,6 +10,7 @@ import RefreshIndicator from 'material-ui/RefreshIndicator';
 import validator from 'validator';
 
 import { getCall } from '../services/api';
+import { postCall } from '../services/api';
 
 const style = {
     container: {
@@ -30,7 +31,6 @@ class addEmployee extends Component {
             name: '',
             doj: '',
             role: '',
-            roleError: '',
             email: '',
             gender: '',
             dob: '',
@@ -38,6 +38,7 @@ class addEmployee extends Component {
             address: '',
             password: '',
             dealer_ID: '',
+            roleError: '',
             dealer_IDError: '',
             passwordError: '',
             addressError: '',
@@ -48,9 +49,8 @@ class addEmployee extends Component {
             dojError: '',
             nameError: '',
             codeError: '',
-            value: 1,
             loading: false,
-            data :[]
+            data: []
         };
 
         this.handleCodeChange = this.handleCodeChange.bind(this);
@@ -58,6 +58,13 @@ class addEmployee extends Component {
         this.handleRoleChange = this.handleRoleChange.bind(this);
         this.handleGenderChange = this.handleGenderChange.bind(this);
         this.handledealerIDChange = this.handledealerIDChange.bind(this);
+        this.handleDojChange = this.handleDojChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handleDobChange = this.handleDobChange.bind(this);
+        this.handleMobileChange = this.handleMobileChange.bind(this);
+        this.handleAddressChange = this.handleAddressChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.add = this.add.bind(this);
     }
 
     componentDidMount() {
@@ -66,7 +73,7 @@ class addEmployee extends Component {
         });
     }
 
-    getDealer(){
+    getDealer() {
         getCall('dealer')
             .then((response) => {
                 console.log('response ::::::::', response.data.dealer);
@@ -81,29 +88,261 @@ class addEmployee extends Component {
     }
 
     handleCodeChange(event) {
-        this.setState({ codeError: event.target.value });
-        // this.handleCodeValidation();
+        this.setState({ code: event.target.value });
+        this.handleCodeValidation();
     }
 
     handleNameChange(event) {
-        this.setState({ nameError: event.target.value });
-        // this.handleNameValidation();
+        this.setState({ name: event.target.value });
+        this.handleNameValidation();
+    }
+
+    handleDojChange(event, date) {
+        this.setState({ doj: (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() });
+        // this.handleDojValidation();
+    }
+
+    handleEmailChange(event) {
+        this.setState({ email: event.target.value });
+        this.handleEmailValidation();
+    }
+
+    handleDobChange(event, date) {
+        this.setState({ dob: (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() });
+        // this.handleDobValidation();
+    }
+
+    handleMobileChange(event) {
+        this.setState({ mobile: event.target.value });
+        this.handleMobileValidation();
+    }
+
+    handleAddressChange(event) {
+        this.setState({ address: event.target.value });
+        this.handleAddressValidation();
+    }
+    handlePasswordChange(event) {
+        this.setState({ password: event.target.value });
+        this.handlePasswordValidation();
     }
 
     handleRoleChange(event, index, value) {
         this.setState({ role: value })
+        this.handleRoleValidation();
     };
 
     handleGenderChange(event, index, value) {
         this.setState({ gender: value })
+        this.handleGenderValidation();
     };
 
     handledealerIDChange(event, index, value) {
-        console.log('value::::::',value);
         this.setState({ dealer_ID: value })
+        this.handledealerIDValidation();
     };
 
+    handleCodeValidation() {
+        if (!validator.isLength(this.state.code, { min: 0 })) {
+            this.setState({ codeError: 'Required' });
+        } else {
+            this.setState({ codeError: '' });
+        }
+    }
 
+    handleNameValidation() {
+        if (!validator.isLength(this.state.name, { min: 0 })) {
+            this.setState({ nameError: 'Required' });
+        } else {
+            this.setState({ nameError: '' });
+        }
+    }
+
+    // handleDojValidation() {
+    //     if (!validator.isLength(this.state.doj, { min: 0 })) {
+    //         this.setState({ dojError: 'Required' });
+    //     } else {
+    //         this.setState({ dojError: '' });
+    //     }
+    // }
+
+    handleEmailValidation() {
+        if (!validator.isEmail(this.state.email)) {
+            this.setState({ emailError: 'email format not correct' })
+        }
+        else {
+            this.setState({ emailError: '' })
+        }
+    }
+
+    // handleDobValidation() {
+    //   if (validator.isAfter(this.state.dob)) {
+    //         this.setState({ dobError: 'not valid date' })
+    //     } else {
+    //         this.setState({ dobError: '' });
+    //     }
+
+    // }
+
+    handleMobileValidation() {
+        if (!validator.isNumeric(this.state.mobile)) {
+            this.setState({ mobileError: 'Mobile should be numeric' })
+
+        }
+        else if (!validator.isLength(this.state.mobile, { min: 9, max: 9 })) {
+            this.setState({ mobileError: 'Length should be 10' })
+        } else {
+            this.setState({ mobileError: '' })
+
+        }
+    }
+
+    handleAddressValidation() {
+        if (!validator.isLength(this.state.address, { min: 0 })) {
+            this.setState({ addressError: 'Required' });
+        } else {
+            this.setState({ addressError: '' });
+        }
+    }
+
+    handlePasswordValidation() {
+        if (!validator.isLength(this.state.password, { min: 5 })) {
+            this.setState({ passwordError: 'Minimum characters is 6' });
+        }
+        else {
+            this.setState({ passwordError: '' });
+        }
+    }
+
+    handleRoleValidation() {
+        if (!validator.isLength(this.state.role, { min: 0 })) {
+            this.setState({ roleError: 'Required' });
+        } else {
+            this.setState({ roleError: '' });
+        }
+    }
+
+    handleGenderValidation() {
+        if (!validator.isLength(this.state.gender, { min: 0 })) {
+            this.setState({ genderError: 'Required' });
+        } else {
+            this.setState({ genderError: '' });
+        }
+
+    }
+
+    handledealerIDValidation() {
+        if (!validator.isLength(this.state.dealer_ID, { min: 0 })) {
+            this.setState({ dealer_IDError: 'Required' });
+        } else {
+            this.setState({ dealer_IDError: '' });
+        }
+    }
+
+    handleValidation() {
+        var errorflag = 0;
+
+        if (!validator.isLength(this.state.code, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ codeError: 'Required' });
+        }
+
+        if (!validator.isLength(this.state.name, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ nameError: 'Required' });
+        }
+
+        // if (!validator.isLength(this.state.doj, { min: 1 })) {
+        //     errorflag = 1;
+        //     this.setState({ dojError: 'Required' });
+        // }
+
+        if (!validator.isEmail(this.state.email)) {
+            errorflag = 1;
+            this.setState({ emailError: 'email format not correct' })
+        }
+
+        // if (!validator.isLength(this.state.dob, { min: 1 })) {
+        //     errorflag = 1;
+        //     this.setState({ dojError: 'Required' });
+        // }
+
+        // if (validator.isAfter(this.state.dob)) {
+        //     errorflag = 1;
+        //     this.setState({ dobError: 'not valid date' })
+        // }
+
+    
+
+        if (!validator.isNumeric(this.state.mobile)) {
+            errorflag = 1;
+            this.setState({ mobileError: 'Mobile should be numeric' })
+        }
+
+        if (!validator.isLength(this.state.mobile, { min: 10, max: 10 })) {
+            errorflag = 1; this.setState({ mobileError: 'Length should be 10' })
+
+        }
+
+        if (!validator.isLength(this.state.address, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ addressError: 'Required' });
+        }
+
+        if (!validator.isLength(this.state.password, { min: 6 })) {
+            errorflag = 1;
+            this.setState({ passwordError: 'Minimum characters is 6' });
+        }
+
+        if (!validator.isLength(this.state.role, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ roleError: 'Required' });
+        }
+
+        if (!validator.isLength(this.state.gender, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ genderError: 'Required' });
+        }
+
+        if (!validator.isLength(this.state.dealer_ID, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ dealer_IDError: 'Required' });
+        }
+
+        if (!validator.isLength(this.state.code, { min: 1 })) {
+            errorflag = 1;
+            this.setState({ codeError: 'Required' });
+        }
+
+
+        if (errorflag == 1)
+            return false;
+        else if (errorflag == 0)
+            return true;
+    }
+
+    clearError() {
+        this.setState({
+            roleError: '',
+            dealer_IDError: '',
+            passwordError: '',
+            addressError: '',
+            mobileError: '',
+            dobError: '',
+            genderError: '',
+            emailError: '',
+            dojError: '',
+            nameError: '',
+            codeError: ''
+        })
+    }
+
+    add() {
+        this.clearError();
+        if (this.handleValidation()) {
+            console.log('hi');
+           
+        }
+    }
 
 
     render() {
@@ -134,13 +373,14 @@ class addEmployee extends Component {
                             floatingLabelText="Select Dealer"
                             value={this.state.dealer_ID}
                             onChange={this.handledealerIDChange}
+                            errorText={this.state.dealer_IDError}
                             maxHeight={400}
-                            // fullWidth={true}
+                        // fullWidth={true}
                         >
 
-                        {this.state.data.map((item) => 
-                                <MenuItem value={item._id} primaryText={item.dealer_Name}/>
-                        )}
+                            {this.state.data.map((item) =>
+                                <MenuItem value={item._id} primaryText={item.dealer_Name} />
+                            )}
 
                         </SelectField>
                         <Row>
@@ -149,8 +389,8 @@ class addEmployee extends Component {
                                     hintText="Employee code"
                                     floatingLabelText="Employee code"
                                     fullWidth={true}
-                                // onChange={this.handleDealerNameChange}
-                                // errorText={this.state.dealerNameError}
+                                    onChange={this.handleCodeChange}
+                                    errorText={this.state.codeError}
                                 />
                             </Col>
                             <Col md="4">
@@ -158,8 +398,8 @@ class addEmployee extends Component {
                                     hintText="Name"
                                     floatingLabelText="Name"
                                     fullWidth={true}
-                                // onChange={this.handleDealerNameChange}
-                                // errorText={this.state.dealerNameError}
+                                    onChange={this.handleNameChange}
+                                    errorText={this.state.nameError}
                                 />
                             </Col>
                             <Col md="4">
@@ -168,9 +408,10 @@ class addEmployee extends Component {
                                     value={this.state.role}
                                     onChange={this.handleRoleChange}
                                     fullWidth={true}
+                                    errorText={this.state.roleError}
                                 >
-                                    <MenuItem value={1} primaryText="Manager" />
-                                    <MenuItem value={2} primaryText="Salesman" />
+                                    <MenuItem value="Manager" primaryText="Manager" />
+                                    <MenuItem value="Salesman"  primaryText="Salesman" />
 
                                 </SelectField>
                             </Col>
@@ -180,11 +421,11 @@ class addEmployee extends Component {
                                 <DatePicker
                                     hintText="doj"
                                     floatingLabelText="Date of Joining"
-                                    // onChange={this.handleDobChange}
+                                    onChange={this.handleDojChange}
                                     fullWidth={true}
                                     openToYearSelection={true}
-                                // locale="en-US"
-                                // errorText={this.state.dobError}
+                                    // locale="en-US"
+                                    errorText={this.state.dojError}
                                 />
                             </Col>
                             <Col md="4">
@@ -192,8 +433,9 @@ class addEmployee extends Component {
                                     hintText="email"
                                     floatingLabelText="Email"
                                     fullWidth={true}
-                                // onChange={this.handleFirstnameChange}
-                                // value={this.state.first_name}
+                                    onChange={this.handleEmailChange}
+                                    errorText={this.state.emailError}
+
                                 />
                             </Col>
                             <Col md="4">
@@ -201,8 +443,8 @@ class addEmployee extends Component {
                                     hintText="Password"
                                     floatingLabelText="Password"
                                     fullWidth={true}
-                                // onChange={this.handleDealerNameChange}
-                                // errorText={this.state.dealerNameError}
+                                    onChange={this.handlePasswordChange}
+                                    errorText={this.state.passwordError}
                                 />
                             </Col>
                         </Row>
@@ -211,11 +453,11 @@ class addEmployee extends Component {
                                 <DatePicker
                                     hintText="dob"
                                     floatingLabelText="Date of Birth"
-                                    // onChange={this.handleDobChange}
+                                    onChange={this.handleDobChange}
                                     fullWidth={true}
                                     openToYearSelection={true}
-                                // locale="en-US"
-                                // errorText={this.state.dobError}
+                                    // locale="en-US"
+                                    errorText={this.state.dobError}
                                 />
                             </Col>
                             <Col md="4">
@@ -223,10 +465,11 @@ class addEmployee extends Component {
                                     floatingLabelText="Gender"
                                     value={this.state.gender}
                                     onChange={this.handleGenderChange}
+                                    errorText={this.state.genderError}
                                     fullWidth={true}
                                 >
-                                    <MenuItem value={1} primaryText="Male" />
-                                    <MenuItem value={2} primaryText="Female" />
+                                    <MenuItem value="male" primaryText="Male" />
+                                    <MenuItem value="female" primaryText="Female" />
 
                                 </SelectField>
                             </Col>
@@ -235,8 +478,8 @@ class addEmployee extends Component {
                                     hintText="Mobile"
                                     floatingLabelText="Mobile"
                                     fullWidth={true}
-                                // onChange={this.handleDealerNameChange}
-                                // errorText={this.state.dealerNameError}
+                                    onChange={this.handleMobileChange}
+                                    errorText={this.state.mobileError}
                                 />
                             </Col>
                         </Row>
@@ -248,8 +491,8 @@ class addEmployee extends Component {
                                     fullWidth={true}
                                     multiLine={true}
                                     rows={2}
-                                // onChange={this.handleDealerNameChange}
-                                // errorText={this.state.dealerNameError}
+                                    onChange={this.handleAddressChange}
+                                    errorText={this.state.addressError}
                                 />
                             </Col>
                         </Row>
@@ -258,7 +501,7 @@ class addEmployee extends Component {
                                 type="button"
                                 label="Add"
                                 primary={true}
-                                // onClick={this.add}
+                                onClick={this.add}
                             />
                         </center>
                     </Col>
